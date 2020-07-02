51fc09ccdd477b8c189b600f608210d50e172061
s.rakeev <s.rakeev@icontext.ru>
2020-07-02 18:12:16 +0300

    add photo download

--- bot_class.py
+++ bot_class.py
@@ -4,25 +4,51 @@ from flask import request
 
 class TelegramBot:
 
+    def __init__(self):
+        self.api_url = False
+        self.post = False
+        self.file_url = False
+
     def login(self, token):
         self.api_url = 'https://api.telegram.org/bot{}/'.format(token)
+        self.file_url = 'https://api.telegram.org/file/bot{}/'.format(token)
 
     def get_updates(self):
         """ Только если не включены WebHooks """
-        url = self.api_url + 'getUpdates'
-        r = requests.get(url)
-        return r.json()
+        if self.api_url:
+            url = self.api_url + 'getUpdates'
+            r = requests.get(url)
+            return r.json()
 
     def send_message(self, chat_id, text):
-        url = self.api_url + 'sendMessage'
-        message = {'chat_id': chat_id, 'text': text}
-        requests.post(url, json=message)
+        if self.api_url:
+            url = self.api_url + 'sendMessage'
+            message = {'chat_id': chat_id, 'text': text}
+            requests.post(url, json=message)
 
-    def get_post(self):
-        self.post = request.get_json()
-        is_bot = self.post['message']['from']['is_bot']
+    def get_json_post(self):
+        r = request.get_json()
+        is_bot = r['message']['from']['is_bot']
         if not is_bot:
+            self.post = r
             return self.post
+
+
+    @property
+    def message_type(self):
+        if self.post:
+            if self.post['message'].get('text'):
+                mtype = 'text'
+            elif self.post['message'].get('photo'):
+                mtype = 'photo'
+            else:
+                mtype = None
+            return mtype
+
+    @property
+    def photo_id(self):
+        if self.message_type == 'photo':
+            return self.post['message']['photo'][-1]['file_id']
 
     @property
     def chat_id(self):
@@ -31,5 +57,17 @@ class TelegramBot:
 
     @property
     def text_message(self):
-        if self.post:
-            return self.post['message']['text']
+        if self.post and self.post['message'].get('text'):
+                return self.post['message']['text']
+
+    def download_file(self, file_id, filename):
+        if self.api_url:
+            url = self.api_url + 'getFile?file_id={}'.format(file_id)
+            r = requests.get(url)
+            json_data = r.json()
+            if json_data.get('ok'):
+                file_path = json_data['result']['file_path']
+                download_url = self.file_url + file_path
+                file = requests.get(download_url)
+                with open('temp_photos/{}.jpg'.format(filename), 'wb') as f:
+                    f.write(file.content)
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import flask
 from flask import request
 from flask import jsonify
 import json
-import requests
+
 
 from bot_class import TelegramBot
 
@@ -13,39 +13,42 @@ with open('token') as f:
 
 def write_json(data, filename='answer.json'):
     with open(filename, 'w', encoding='utf-8') as f:
-        json.dump(data, f, indent=2)
-
-
-def main():
-    # updates = get_updates()
-    # chat_id = get_last_chat_id(updates)
-    # send_message(chat_id, 'Hello')
-    # bot = TelegramBot()
-    # bot.login(TOKEN)
-    # r = bot.get_updates()
-    # print(r)
-    pass
+        json.dump(data, f, indent=2, ensure_ascii=False)
 
 
 app = flask.Flask(__name__)
 bot = TelegramBot()
 bot.login(TOKEN)
+last_message_on_chat_id = {}
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        # r = request.get_json()
-        # chat_id = bot.get_chat_id_from_json(r)
-        r = bot.get_post()
-        print(bot.chat_id)
-        print(bot.text_message)
-        # write_json(r)
+        r = bot.get_json_post()
+        print(last_message_on_chat_id)
+
+        if bot.message_type == 'text':
+            if bot.text_message == '/start':
+                last_message_on_chat_id[bot.chat_id] = 1
+                bot.send_message(bot.chat_id, 'Приветсвую. Отправьте первую фотографию')
+
+        elif bot.message_type == 'photo':
+            if last_message_on_chat_id.get(bot.chat_id) == 1:
+                last_message_on_chat_id[bot.chat_id] = 2
+                bot.download_file(bot.photo_id, '{}_1'.format(bot.chat_id))
+                bot.send_message(bot.chat_id, 'Отправьте вторую фотографию')
+            elif last_message_on_chat_id.get(bot.chat_id) == 2:
+                last_message_on_chat_id[bot.chat_id] = 3
+                bot.download_file(bot.photo_id, '{}_2'.format(bot.chat_id))
+                bot.send_message(bot.chat_id, 'Подождите результат')
+
         return jsonify(r)
+
     return '<h1>Hello My app</h1>'
 
 
+
 if __name__ == '__main__':
-    # main()
     app.run()
 
